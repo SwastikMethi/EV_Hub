3de0f639aff24aa079eb1a73fe9f41750a01df17
SwastikMethi <smethi05@gamil.com>
2024-04-07 04:06:05 +0530

    started map implementation

--- app/src/main/java/com/example/ev_hub/Activity_Map.java
+++ app/src/main/java/com/example/ev_hub/Activity_Map.java
@@ -2,7 +2,6 @@ package com.example.ev_hub;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,21 +9,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Activity_HomePage extends AppCompatActivity {
+public class Activity_Map extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_map);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        ImageButton map = findViewById(R.id.nav_button);
-        map.setOnClickListener(view ->{
-            Intent intent = new Intent(Activity_HomePage.this, Activity_Map.class);
+        findViewById(R.id.home_button).setOnClickListener(view ->{
+            Intent intent = new Intent(Activity_Map.this, Activity_HomePage.class);
             startActivity(intent);
         } );
     }
