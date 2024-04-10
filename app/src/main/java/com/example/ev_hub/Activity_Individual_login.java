cb7f3173c72be46c52a5e9f1317bd78fa614e73c
SwastikMethi <smethi05@gamil.com>
2024-04-10 13:16:55 +0530

    Registration and Login completed from firebase

--- app/src/main/java/com/example/ev_hub/Activity_Individual_login.java
+++ app/src/main/java/com/example/ev_hub/Activity_Individual_login.java
@@ -1,16 +1,33 @@
 package com.example.ev_hub;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Activity_Individual_login extends AppCompatActivity {
+    EditText email, password;
+    Button btn_login;
+    CheckBox remember_me;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +44,51 @@ public class Activity_Individual_login extends AppCompatActivity {
             Intent intent = new Intent(Activity_Individual_login.this,Activity_Individual_register.class);
             startActivity(intent);
         } );
+
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.email_login);
+        password = findViewById(R.id.password_login);
+        btn_login = findViewById(R.id.login_button);
+        remember_me = findViewById(R.id.checkbox);
+        SharedPreferences sharedPreferences = getSharedPreferences("user_data",MODE_PRIVATE);
+
+        if(sharedPreferences.contains("email") && sharedPreferences.contains("password")){
+            Intent intent = new Intent(Activity_Individual_login.this,Activity_HomePage.class);
+            startActivity(intent);
+        }
+
+        btn_login.setOnClickListener(view ->{
+            String email_text = email.getText().toString();
+            String password_text = password.getText().toString();
+            if(TextUtils.isEmpty(email_text)){
+                Toast.makeText(this, "Email is required", Toast.LENGTH_SHORT).show();
+            }
+            if(TextUtils.isEmpty(password_text)){
+                Toast.makeText(this, "Password is required", Toast.LENGTH_SHORT).show();
+            }
+
+            if(remember_me.isChecked()){
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("email",email_text);
+                editor.putString("password",password_text);
+                editor.apply();
+            }
+            mAuth.signInWithEmailAndPassword(email_text, password_text)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                //FirebaseUser user = mAuth.getCurrentUser();
+                                Intent intent = new Intent(Activity_Individual_login.this,Activity_HomePage.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(Activity_Individual_login.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        });
     }
 }
--- app/src/main/java/com/example/ev_hub/Activity_Individual_register.java
+++ app/src/main/java/com/example/ev_hub/Activity_Individual_register.java
@@ -1,16 +1,38 @@
 package com.example.ev_hub;
 
+import static android.app.ProgressDialog.show;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
 public class Activity_Individual_register extends AppCompatActivity {
+
+    EditText name, email, password;
+    Button btn_register;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +49,62 @@ public class Activity_Individual_register extends AppCompatActivity {
         login_click.setOnClickListener(view ->{
             Intent intent = new Intent(Activity_Individual_register.this,Activity_Individual_login.class);
             startActivity(intent);
+            finish();
         } );
+
+        mAuth = FirebaseAuth.getInstance();
+        name = findViewById(R.id.full_name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btn_register = findViewById(R.id.register_button);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, name, password;
+                name = String.valueOf(Activity_Individual_register.this.name.getText());
+                email = String.valueOf(Activity_Individual_register.this.email.getText());
+                password = String.valueOf(Activity_Individual_register.this.password.getText());
+
+                if(TextUtils.isEmpty(name)){
+                    Toast.makeText(Activity_Individual_register.this, "Name is required", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(Activity_Individual_register.this, "Email is required", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(Activity_Individual_register.this, "Password is required", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length()<8){
+                    TextView alert = findViewById(R.id.alert);
+                    alert.setVisibility(View.VISIBLE);
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    //FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(Activity_Individual_register.this, "Account created successfully", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(Activity_Individual_register.this, Activity_Individual_login.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Activity_Individual_register.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
